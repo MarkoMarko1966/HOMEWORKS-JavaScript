3e38be6840a03336169accdea2030fe389ac670d
MarkoMarko1966 <115075407+MarkoMarko1966@users.noreply.github.com>
2022-12-12 17:38:48 +0100

    Add files via upload

---
+++ HOMEWORK 1/script.js
@@ -0,0 +1,19 @@
+//zadaca 1
+
+var cur_age = 23; // 67 * 10 * 365
+var max_age = 90;
+var max_eat = 10;
+console.log(`You will need ${(max_age - cur_age) * max_eat * 365} to last you until the ripe old age of ${max_age}.`);
+
+// zadaca 2
+
+var  r = 10;
+console.log(`The circumference is ${r * 2}.`);
+console.log(`The area is ${Math.pow(r, 2) * Math.PI}.`);
+
+//zadaca 3
+
+var cel = 20;
+console.log(`${cel}°C is ${cel / 5 * 9 + 32}°F.`);
+var fah = 68;
+console.log(`${fah}°F is ${(fah - 32) * 5 / 9}°C.`);
